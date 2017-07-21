bd66043bed7250f2b864b431dcc1992901538341
Isamu Arimoto <isamu@to-kyo.to>
2017-07-21 17:19:44 +0900

    v 1.0.7

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ var concat = require('gulp-concat');
 var runSequence = require('run-sequence');
  
 
-var version = "1.0.7";
+var version = "1.0.8";
 
 gulp.task("babel", function () {
     return gulp.src("src/js/*.js")
